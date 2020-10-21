6ed403256fd3d8399f507725b9dd9ac0ecb7f7a5
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-21 13:17:33 +0100

    added functionality to plot results of multiple models on the same figure

--- pkmodel/plotting.py
+++ pkmodel/plotting.py
@@ -26,4 +26,26 @@ def plot_singlemodel(model, sol):
     return fig
 
 
+def plot_multiplemodels(model_list, sol_list):
+    '''
+    A function to plot multiple model solutions on the same figure
 
+    :param model_list: A list of model class instances
+    :type model_list: list
+    :param sol: an list of objects (sol) containing sol.t, an array of timesteps; and sol.y, an array of the ODE solutions
+    :type sol: list
+    :return fig: DESCRIPTION
+    :rtype: matplotlib figure
+
+    '''
+    fig = plt.figure()
+
+    for j in range(0, len(model_list)):
+        for i in range(0, model_list[j].compartments, 1):
+            plt.plot(sol_list[j].t, sol_list[j].y[i, :], label="Model: " + model_list[j].name + " Compartments " + i)
+
+    plt.legend()
+    plt.ylabel('Drug Mass [ng]')
+    plt.xlabel('Time [hr]')
+
+    return fig
