cc05ba1246047e5834f4751c992c3b468622f8e3
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-22 12:23:27 +0100

    #16 Add validation for model inputs

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,6 +1,23 @@
 #
 # Model class
 #
+def validate(name, params, compartments, protocol):
+    for key in params:
+        if (type(params[key]) is not float):
+            if (type(params[key]) is not int):
+                raise TypeError('All parameters should be floating point numbers (e.g. 1.0). Parameter ' + key + ' is not.')
+        if params[key]<=0.:
+            raise ValueError('Values must all be greater than 0')
+
+    if protocol not in ['ivb','sc']:
+        raise ValueError("Protocol must be 'ivb' or 'sc'")
+    if (type(compartments) is not float):
+        if (type(compartments) is not int):
+            raise TypeError('Number of compartments should be a floating point number (e.g. 2.0)')
+    if compartments<=0:
+        raise ValueError('Number of compartments must be 1 or more')
+    if type(name) is not str:
+        raise TypeError('Name must be a string')
 
 class Model:
 
@@ -11,14 +28,21 @@ class Model:
         :type name: string
         :param compartments: number of compartments
         :type compartments: integer
-        :param protocol: enter 0 for IVB or 1 for subcutaneous
+        :param protocol: enter 'ivb' for IVB or 'sc' for subcutaneous
         :type protocol: integer
 
         '''
-
+        validate(name, params, compartments, protocol)
         self.name = name
         self.compartments = compartments
         self.protocol = protocol
         self.params = params
+
+        
+
+
+
+
+    
 
 
