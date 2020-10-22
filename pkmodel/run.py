f57f84f37aba2ab89d5b33efb6246c914b38b9ef
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-22 12:22:28 +0100

    rearrange params

--- pkmodel/run.py
+++ pkmodel/run.py
@@ -14,29 +14,28 @@ import solve_model
 # USER INPUTS
 
 params1 = {
-            'Q_p1': 1.0,
             'V_c': 1.0,
-            'V_p1': 1.0,
             'CL': 1.0,
             'k_a': 1.5,
             'dose_rate': 2.0,
             't_dose': 3.0,
-            't_end': 10.0
+            't_end': 10.0,
+            'Q_p1': 1.0,
+            'V_p1': 1.0
         }
 
 params2 = {
-            'Q_p1': 2.0,
             'V_c': 13.0,
-            'V_p1': 1.0,
             'CL': 1.0,
             'k_a': 1.5,
             'dose_rate': 2.0,
             't_dose': 3.0,
-            't_end': 10.0
+            't_end': 10.0,
+            'Q_p1': 2.0,
+            'V_p1': 1.0
         }
 
 model_list = [Model('example_name', params1, 1, 'ivb'), Model('example_name2', params2, 2, 'sc')]
-
 # FIND SOLUTIONS
 sol_list = []
 for model in model_list:
