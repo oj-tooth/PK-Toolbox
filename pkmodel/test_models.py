f2a6362d6a2a9968a977b4f192b88b12d388a9a7
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-22 17:37:13 +0100

    linted with flake8

--- pkmodel/test_models.py
+++ pkmodel/test_models.py
@@ -2,42 +2,45 @@ from model import Model
 import pytest
 import numpy
 
+
 @pytest.fixture
 def model_init():
     params = {
-    'Q_p1': 1.0,
-    'V_c': 1.0,
-    'V_p1': 1.0,
-    'CL': 1.0,
-    'k_a': 1.5,
-    'dose_rate': 2.0,
-    't_end': 10.0,
-    't_dose': 3
+        'Q_p1': 1.0,
+        'V_c': 1.0,
+        'V_p1': 1.0,
+        'CL': 1.0,
+        'k_a': 1.5,
+        'dose_rate': 2.0,
+        't_end': 10.0,
+        't_dose': 3
     }
-    return Model('test_model', params, compartments=1) 
+    return Model('test_model', params, compartments=1)
+
 
 @pytest.fixture
 def model_init_2comp():
     params = {
-    'Q_p1': 1.0,
-    'V_c': 1.0,
-    'V_p1': 1.0,
-    'CL': 1.0,
-    'k_a': 1.5,
-    'dose_rate': 2.0,
-    't_end': 10.0,
-    't_dose': 3
+        'Q_p1': 1.0,
+        'V_c': 1.0,
+        'V_p1': 1.0,
+        'CL': 1.0,
+        'k_a': 1.5,
+        'dose_rate': 2.0,
+        't_end': 10.0,
+        't_dose': 3
     }
-    return Model('test_model', params, compartments=2) 
+    return Model('test_model', params, compartments=2)
 
 
 def test_solve_output_type(model_init):
-    
+
     import solve_model
     assert isinstance(solve_model.solve(model_init).y, numpy.ndarray)
 
 
 def test_solve_output_shape(model_init):
-    
+
     import solve_model
-    assert solve_model.solve(model_init).y.shape[0,:] == solve_model.solve(model_init).t.shape[0,:]
+    assert solve_model.solve(model_init).y.shape[0, :] == \
+        solve_model.solve(model_init).t.shape[0, :]
