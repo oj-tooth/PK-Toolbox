9faffac59305949b5f0f36451c0c284e0284272c
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-22 12:34:59 +0100

    refactor validation

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,23 +1,7 @@
 #
 # Model class
 #
-def validate(name, params, compartments, protocol):
-    for key in params:
-        if (type(params[key]) is not float):
-            if (type(params[key]) is not int):
-                raise TypeError('All parameters should be floating point numbers (e.g. 1.0). Parameter ' + key + ' is not.')
-        if params[key]<=0.:
-            raise ValueError('Values must all be greater than 0')
-
-    if protocol not in ['ivb','sc']:
-        raise ValueError("Protocol must be 'ivb' or 'sc'")
-    if (type(compartments) is not float):
-        if (type(compartments) is not int):
-            raise TypeError('Number of compartments should be a floating point number (e.g. 2.0)')
-    if compartments<=0:
-        raise ValueError('Number of compartments must be 1 or more')
-    if type(name) is not str:
-        raise TypeError('Name must be a string')
+import validate
 
 class Model:
 
@@ -32,7 +16,7 @@ class Model:
         :type protocol: integer
 
         '''
-        validate(name, params, compartments, protocol)
+        validate.validate(name, params, compartments, protocol)
         self.name = name
         self.compartments = compartments
         self.protocol = protocol
