8dc9fc1f8049698fc43984aabdab4dc085c7f35a
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-22 13:54:09 +0100

    #16 test validation of model inputs

---
+++ pkmodel/tests/test_validation.py
@@ -0,0 +1,115 @@
+import pytest
+@pytest.mark.parametrize(
+
+    "test, raises",
+    [
+        (
+            'test_model',
+            None,
+        ),
+        (
+            '',
+            None,
+        ),
+        (
+            3,
+            TypeError,
+        ),
+        (
+            ['this','is','a','list'],
+            TypeError,
+        )
+    ])
+def test_validate_name(test, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from validate import validate_name
+    if raises:
+        with pytest.raises(raises):
+            validate_name(test)
+
+
+@pytest.mark.parametrize(
+
+    "test, raises",
+    [
+        (
+            'ivb',
+            None,
+        ),
+        (
+            'sc',
+            None,
+        ),
+        (
+            3,
+            TypeError,
+        ),
+        (
+            'hello',
+            ValueError,
+        )
+    ])
+def test_validate_protocol(test, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from validate import validate_name
+    if raises:
+        with pytest.raises(raises):
+            validate_protocol(test)
+
+
+@pytest.mark.parametrize(
+
+    "test, raises",
+    [
+        (
+            1,
+            None,
+        ),
+        (
+            -10,
+            ValueError,
+        ),
+        (
+            0,
+            ValueError,
+        ),
+        (
+            'test',
+            TypeError,
+        )
+    ])
+def test_validate_compartments(test, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from validate import validate_name
+    if raises:
+        with pytest.raises(raises):
+            validate_compartments(test)
+
+
+@pytest.mark.parametrize(
+
+    "test, raises",
+    [
+        (
+            {'test':1},
+            None,
+        ),
+        (
+            {'test':0},
+            ValueError,
+        ),
+        (
+            {'test':-10},
+            ValueError,
+        ),
+        (
+            {'test':'string'},
+            TypeError,
+        )
+    ])
+def test_validate_name(test, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from validate import validate_name
+    if raises:
+        with pytest.raises(raises):
+            validate_name(test)
