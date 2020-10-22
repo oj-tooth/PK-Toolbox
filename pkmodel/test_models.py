5f91b079b1d7a5ea89b84c1a36a0f7210495f2b5
Rhidian Thomas <rhidian.thomas@env-res.ox.ac.uk>
2020-10-22 17:11:51 +0100

    Test that solution outputs numpy array #3

--- pkmodel/test_models.py
+++ pkmodel/test_models.py
@@ -1,6 +1,6 @@
 from model import Model
 import pytest
-
+import numpy
 
 @pytest.fixture
 def model_init():
@@ -11,27 +11,33 @@ def model_init():
     'CL': 1.0,
     'k_a': 1.5,
     'dose_rate': 2.0,
-    't_end': 10.0
+    't_end': 10.0,
+    't_dose': 3
     }
     return Model('test_model', params, compartments=1) 
-def test_solve(model_init):
-    
+
+@pytest.fixture
+def model_init_2comp():
+    params = {
+    'Q_p1': 1.0,
+    'V_c': 1.0,
+    'V_p1': 1.0,
+    'CL': 1.0,
+    'k_a': 1.5,
+    'dose_rate': 2.0,
+    't_end': 10.0,
+    't_dose': 3
+    }
+    return Model('test_model', params, compartments=2) 
+
+
+def test_solve_output_type(model_init):
     
     import solve_model
-    print(solve_model.solve(model_init))
-        
+    assert isinstance(solve_model.solve(model_init).y, numpy.ndarray)
 
+
+def test_solve_output_shape(model_init):
     
-
-
-"""
-def test_rhs(test, expected, raises):
-    
-    from solve_model import rhs
-
-    #if raises:
-    #    with pytest.raises(raises):
-    #        # Wait until I have an actual error to raise here
-    #else:
-    assert expected == rhs(test)
-"""
+    import solve_model
+    assert solve_model.solve(model_init).y.shape[0,:] == solve_model.solve(model_init).t.shape[0,:]
