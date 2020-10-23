8cece61af49f780ab620d7e598fe5363c450a1b3
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-23 19:11:30 +0100

    change plotting import

--- pkmodel/__init__.py
+++ pkmodel/__init__.py
@@ -10,6 +10,6 @@ from .version_info import VERSION_INT, VERSION # noqa
 # Import main modules.
 from .model import *  # noqa
 from .solve_model import * # noqa
-import .plotting  # noqa
+import plotting  # noqa
 from .validate import * # noqa
 from .run_PK import * # noqa
