8d5e73b4e4fca157a845e49fd51235667fdf8168
RebeccaColquhoun <rcolquhoun13@gmail.com>
2020-10-23 17:38:06 +0100

    linted with flake8

--- pkmodel/__init__.py
+++ pkmodel/__init__.py
@@ -1,12 +1,15 @@
-
-"""PK Toolbox is Pharmokinetic Model and Visualisation Toolbox! 
 """
+PK Toolbox is Pharmokinetic Model and Visualisation Toolbox!
+"""
+
+
 # Import version info
 from .version_info import VERSION_INT, VERSION  # noqa
 
+
 # Import main classes
 import .model    # noqa
-import .solve_model #noqa
-import .plotting # noqa
-import .validate # noqa
+import .solve_model  #noqa
+import .plotting  # noqa
+import .validate  # noqa
 import .run_PK  # noqa
