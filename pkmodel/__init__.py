72835dc2462689117acabaf55222b51eb0f91afb
RebeccaColquhoun <rcolquhoun13@gmail.com>
2020-10-23 18:03:54 +0100

    Update __init__.py

--- pkmodel/__init__.py
+++ pkmodel/__init__.py
@@ -5,7 +5,7 @@ PK Toolbox is Pharmokinetic Model and Visualisation Toolbox!
 
 # Import version info
 # from version_info import VERSION_INT, VERSION
-import version_info # noqa
+# import version_info # noqa
 
 
 # Import main classes
