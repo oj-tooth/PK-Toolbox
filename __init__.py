4911a9974c02c5a9dbbaf74c9a1c6152bd3f91e0
Ollie Tooth <72970877+oj-tooth@users.noreply.github.com>
2020-10-23 18:24:06 +0100

    Created __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""
+PK Toolbox is Pharmokinetic Model and Visualisation Toolbox!
+"""
+
+
+# Import version info
+# from version_info import VERSION_INT, VERSION
+# import version_info # noqa
+
+
+# Import main classes
+from pkmodel.model import *  # noqa
+from pkmodel.solve_model import * # noqa
+import pkmodel.plotting  # noqa
+from pkmodel.validate import * # noqa
+from pkmodel.run_PK import * # noqa
