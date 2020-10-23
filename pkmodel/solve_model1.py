566a501a3dd7015cb6f7ea8cb09a786fe9e3b58b
lk1516 <lk1516@ic.ac.uk>
2020-10-23 11:51:53 +0100

    More solve_model1 docstring testing

--- pkmodel/solve_model1.py
+++ pkmodel/solve_model1.py
@@ -3,8 +3,20 @@ import scipy.integrate
 
 
 def dose(t, dose_rate, t_dose):
-    '''Test docstring
     '''
+    Model class object containing the model name and various parameters.
+    Takes mandatory arguments name and params and optional arguments
+    compartments and protocol.
+
+    :param name: name of model
+    :type name: string
+    :param compartments: number of compartments
+    :type compartments: integer
+    :param protocol: enter 'ivb' for IVB or 'sc' for subcutaneous
+    :type protocol: integer
+    
+    '''
+
     return dose_rate * np.heaviside(t_dose - t, 1)
 
 
