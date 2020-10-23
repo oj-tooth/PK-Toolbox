8b733fe54a74078078831087f490ef7f5d96a646
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-23 19:02:28 +0100

    added init.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""pkmodel is a Pharmokinetic modelling library.
+It contains functionality for creating, solving, and visualising the solution
+of Parmokinetic (PK) models
+"""
+# Import version info
+#from .version_info import VERSION_INT, VERSION  # noqa
+
+import run_PK
